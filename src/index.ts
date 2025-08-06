21d0aeac7ea20f7ded7d91b9e071363d67ef45b3
Gold Edem Hogan <edge37@outlook.com>
2025-08-06 13:55:13 +0100

    All Glory Be To God - DO makes sends POST request to SvelteKit site to save message to db - didn't work because of CORS

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,13 @@
 import { DurableObject } from 'cloudflare:workers';
 
-export interface Env {
-	R: DurableObjectNamespace<R>;
-}
-
 export class R extends DurableObject<Env> {
 	constructor(ctx: DurableObjectState, env: Env) {
 		super(ctx, env);
-		this.ctx = ctx
+		this.ctx = ctx;
 	}
 
 	async send(data: object) {
-		console.log(this.ctx.getWebSockets())
+		console.log(this.ctx.getWebSockets());
 		this.ctx.getWebSockets().forEach((ws) => {
 			console.log('sending to', ws);
 			ws.send(JSON.stringify(data));
@@ -30,10 +26,67 @@ export class R extends DurableObject<Env> {
 		});
 	}
 
-	async webSocketMessage(ws: WebSocket, message: string | ArrayBuffer) {
+	async webSocketMessage(ws: WebSocket, body: string | ArrayBuffer) {
 		this.ctx.getWebSockets().forEach((ws) => {
-			ws.send(message);
+			ws.send(body);
 		});
+		try {
+			let dataString: string;
+			if (typeof body === 'string') {
+				dataString = body;
+			} else {
+				dataString = new TextDecoder().decode(body);
+			}
+			const data = JSON.parse(dataString);
+			try {
+				const response = await fetch(this.env.i, {
+					method: 'POST',
+					body,
+				});
+
+				if (!response.ok) {
+					let respText: string | undefined;
+					try {
+						respText = await response.text();
+					} catch (_e) {
+						respText = undefined;
+					}
+					const info = {
+						error: 'HTTP error',
+						status: response.status,
+						statusText: response.statusText,
+						request: {
+							method: 'POST',
+							url: this.env?.i,
+							rawBody: typeof body === 'string' ? body : '[ArrayBuffer]',
+							dataString,
+							data,
+						},
+						response: {
+							ok: response.ok,
+							status: response.status,
+							statusText: response.statusText,
+							headers: Object.fromEntries(response.headers.entries()),
+							bodyText: respText,
+						},
+					};
+					throw new Error(JSON.stringify(info, null, 2));
+				}
+			} catch (e) {
+				if (e instanceof Error) {
+					console.error('r message save:', e.message, {
+						name: e.name,
+						stack: e.stack,
+						cause: (e as any).cause,
+					});
+				} else {
+					console.error('r message save: Non-Error thrown value', e);
+				}
+			}
+		} catch (e) {
+			// This will now catch both JSON parsing errors and fetch-related errors
+			console.error('An error occurred:', e.message);
+		}
 	}
 
 	async webSocketClose(ws: WebSocket, code: number, reason: string, wasClean: boolean) {
